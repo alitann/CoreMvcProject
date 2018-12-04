116b828a95c8e505f18fdf25399b6198d10b9a8a
alitann <benalitan@gmail.com>
2018-12-04 22:51:00 +0300

    ProductCreateViewModel Added

---
+++ MvcCoreSample/MvcCoreSample/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcCoreSample.Models;
+using System.Collections.Generic;
+
+namespace MvcCoreSample.ViewModels
+{
+    public class ProductCreateViewModel
+    {
+        public Product Product { get; set; }
+
+        public IFormFile FormFile { get; set; }
+
+        public List<SelectListItem> Brands { get; set; }
+
+        public string BrandId { get; set; }
+    }
+}
